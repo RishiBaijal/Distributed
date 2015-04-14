96e89cf3ee8ce017ba6d5fd6047c25ae84c2883b
Rishi Baijal <rishi12084@iiitd.ac.in>
2015-04-14 20:02:32 +0530

    Made some changes

--- FileSharing/app/src/main/java/com/example/rishi/filesharing/MainActivity.java
+++ FileSharing/app/src/main/java/com/example/rishi/filesharing/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.rishi.filesharing;
 
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,10 +11,25 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private final IntentFilter intentFilter = new IntentFilter();
+    WifiP2pManager.Channel mChannel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // We are listening for the following actions to happen listening intent filters
+
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        WifiP2pManager mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+
+
     }
 
 
